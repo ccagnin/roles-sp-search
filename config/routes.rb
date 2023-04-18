19bfc27ebc8b326327b1e13dbec84ff4d5e72df5
ccagnin <cagnin.caroline@gmail.com>
2023-04-17 22:26:30 -0300

    create registrations controller and fix sessions controller bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'places#index'
   resources :places, only: [:index, :show, :create]
   resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
 end
 
