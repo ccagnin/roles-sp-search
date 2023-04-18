895f8cdeceffc41702e44dee5b6d1c1e8ef86eef
ccagnin <cagnin.caroline@gmail.com>
2023-04-17 23:11:35 -0300

    session store fix config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
-Rails.application.config.session_store :cookie_store, key: "_roles_sp_app", domain: "localhost:3000"
+Rails.application.config.session_store :cookie_store, key: "_roles_sp_app"
+# Rails.application.config.session_store :cookie_store, key: "_roles_sp_app", domain: "production-roles-sp-app.herokuapp.com"
+
