e6c7753106f1da41ee618f2cf0768e09cfff9192
ccagnin <cagnin.caroline@gmail.com>
2023-04-17 23:04:21 -0300

    create logged_in status and logout sessions method

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :places, only: [:index, :show, :create]
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
 end
-
