e4f6aa912f87547fcad18fcf60a5a8d0a5f125a8
ccagnin <cagnin.caroline@gmail.com>
2023-04-17 23:54:23 -0300

    create home and places page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { connect } from 'react-redux'
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/Home'
+import Places from './components/Places'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/places' element={<Places />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default connect(
-  state=>({
+export default App;
 
-  }),
-  {}
-)(App)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store.js'
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>
-, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+);
 serviceWorker.unregister();
+
