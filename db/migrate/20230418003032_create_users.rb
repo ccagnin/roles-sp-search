29ef3f1917c7c11324a25c7f84812d99b1dcca1a
ccagnin <cagnin.caroline@gmail.com>
2023-04-17 21:40:01 -0300

    create user model

--- db/migrate/20230418003032_create_users.rb
+++ db/migrate/20230418003032_create_users.rb
@@ -1,9 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
+    drop_table :users
     create_table :users do |t|
-      t.string :name
       t.string :email
       t.string :password_digest
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_05_225015) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_003032) do
   create_table "places", force: :cascade do |t|
     t.string "name"
     t.string "place_type"
@@ -23,9 +23,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_05_225015) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
