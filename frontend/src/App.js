b084763b79c038b67b3dff9f1c5b9d860ddddb46
ccagnin <cagnin.caroline@gmail.com>
2023-04-24 21:20:09 -0300

    check if user is logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import Home from './components/Home'
 import Places from './components/Places'
 
@@ -14,6 +15,31 @@ export default class App extends Component {
     this.handleLogin = this.handleLogin.bind(this);
 
   }
+
+  checkLoginStatus() {
+    axios.get('http://localhost:4000/logged_in', { withCredentials: true })
+      .then(response => {
+        if (response.data.logged_in && this.state.loggedInStatus === 'NOT_LOGGED_IN') {
+          this.setState({
+            loggedInStatus: 'LOGGED_IN',
+            user: response.data.user
+          })
+        } else if (!response.data.logged_in && this.state.loggedInStatus === 'LOGGED_IN') {
+          this.setState({
+            loggedInStatus: 'NOT_LOGGED_IN',
+            user: {}
+          })
+        }
+      })
+      .catch(error => {
+        console.log('check login error', error);
+      });
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
 
   handleLogin(data) {
     this.setState({
